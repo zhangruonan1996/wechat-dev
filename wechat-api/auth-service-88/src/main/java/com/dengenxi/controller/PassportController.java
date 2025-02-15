39ec33135f50375573158318469b18a6bbeb2caa
coderqin <coderqin@foxmail.com>
2025-02-15 19:07:26 +0800

    使用拦截器限制60秒短信发送

--- wechat-api/auth-service-88/src/main/java/com/dengenxi/controller/PassportController.java
+++ wechat-api/auth-service-88/src/main/java/com/dengenxi/controller/PassportController.java
@@ -3,6 +3,7 @@ package com.dengenxi.controller;
 import com.dengenxi.base.BaseInfoProperties;
 import com.dengenxi.grace.result.GraceJSONResult;
 import com.dengenxi.tasks.SmsTask;
+import com.dengenxi.utils.IPUtil;
 import com.dengenxi.utils.MyInfo;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
@@ -30,11 +31,17 @@ public class PassportController extends BaseInfoProperties {
         if (StringUtils.isBlank(mobile)) {
             return GraceJSONResult.error();
         }
+
+        // 获取用户的手机号/IP
+        String userIP = IPUtil.getRequestIp(request);
+        // 限制该用户的手机号/IP，在60秒内只能获得一次验证码
+        redis.setnx60s(MOBILE_SMSCODE + ":" + userIP, mobile);
+
         String code = String.valueOf((int) ((Math.random() * 9 + 1) * 100000));
         smsTask.sendSmsInTask(MyInfo.getMobile(), code);
 
         // 把验证码存入redis，用于后续的注册/登录的校验
-        redis.set(MOBILE_SMSCODE + ":" + mobile, code, 30 * 60);
+        redis.set(MOBILE_SMSCODE + ":" + mobile, code, 60);
 
         return GraceJSONResult.ok();
     }
