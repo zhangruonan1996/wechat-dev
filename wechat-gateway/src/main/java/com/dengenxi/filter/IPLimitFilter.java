d30c131c09a8af92ee1fcabb295e2a8212ac24f3
coderqin <coderqin@foxmail.com>
2025-02-15 15:03:16 +0800

    网关高并发流量防刷-防刷逻辑落地

--- wechat-gateway/src/main/java/com/dengenxi/filter/IPLimitFilter.java
+++ wechat-gateway/src/main/java/com/dengenxi/filter/IPLimitFilter.java
@@ -3,6 +3,7 @@ package com.dengenxi.filter;
 import com.dengenxi.base.BaseInfoProperties;
 import com.dengenxi.grace.result.GraceJSONResult;
 import com.dengenxi.grace.result.ResponseStatusEnum;
+import com.dengenxi.utils.IPUtil;
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
@@ -10,6 +11,7 @@ import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MimeTypeUtils;
@@ -36,14 +38,64 @@ public class IPLimitFilter extends BaseInfoProperties implements GlobalFilter, O
      * 等待30秒静默后，才能够继续恢复访问
      */
 
+    private static final Integer CONTINUE_COUNTS = 3;
+    private static final Integer TIME_INTERVAL = 20;
+    private static final Integer LIMIT_TIMES = 30;
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        log.info("IP 当前的执行顺序order为1");
-        if (true) {
-            // 终止请求，返回错误信息
+        // if (true) {
+        //     // 终止请求，返回错误信息
+        //     return renderErrorMsg(exchange, ResponseStatusEnum.SYSTEM_ERROR_BLACK_IP);
+        // }
+
+        return doLimit(exchange, chain);
+
+        // 默认放行请求到后续的路由（服务）
+        // return chain.filter(exchange);
+    }
+
+    /**
+     * 限制IP请求次数的判断
+     *
+     * @param exchange
+     * @param chain
+     * @return
+     * @author qinhao
+     * @email coderqin@foxmail.com
+     * @date 2025-02-15 14:46:06
+     */
+    public Mono<Void> doLimit(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 根据request获取请求IP
+        ServerHttpRequest request =  exchange.getRequest();
+        String ip = IPUtil.getIP(request);
+
+        // 正常的ip定义
+        final String ipRedisKey = "gateway-ip:" + ip;
+        // 被拦截的黑名单IP，如果在redis中存在，则表示目前被关小黑屋
+        final String ipRedisLimitKey = "gateway-ip:limit" + ip;
+
+        // 获得当前的IP并且查询还剩下多少时间，如果时间存在（大于0），则表示当前仍然处在黑名单中
+        long limitLeftTimes = redis.ttl(ipRedisLimitKey);
+        if (limitLeftTimes > 0) {
+            // 终止请求，返回错误
             return renderErrorMsg(exchange, ResponseStatusEnum.SYSTEM_ERROR_BLACK_IP);
         }
-        // 默认放行请求到后续的路由（服务）
+
+        // 在redis中获得IP的累加次数
+        long requestCounts = redis.increment(ipRedisKey, 1);
+        // 如果是第一次访问，也就是从0开始计数，则初次访问就是1
+        if (requestCounts == 1) {
+            redis.expire(ipRedisKey, TIME_INTERVAL);
+        }
+        // 如果还能获得请求的正常次数，说明用户的连续请求落在限定的次数之类
+        // 一旦请求次数超过限定的连续访问次数，则需要限制当前的IP
+        if (requestCounts > CONTINUE_COUNTS) {
+            // 限制IP访问的时间
+            redis.set(ipRedisLimitKey, ipRedisLimitKey, LIMIT_TIMES);
+            // 终止请求，返回错误
+            return renderErrorMsg(exchange, ResponseStatusEnum.SYSTEM_ERROR_BLACK_IP);
+        }
         return chain.filter(exchange);
     }
 
