82928c03e79e5a0b18061fca210ea70e62220b3c
coderqin <coderqin@foxmail.com>
2025-03-17 21:38:27 +0800

    Netty服务的生命周期

--- wechat-api/chat-server-976/src/main/java/org/zhangruonan/netty/HttpHandler.java
+++ wechat-api/chat-server-976/src/main/java/org/zhangruonan/netty/HttpHandler.java
@@ -26,21 +26,84 @@ public class HttpHandler extends SimpleChannelInboundHandler<HttpObject> {
         // 获取channel
         Channel channel = channelHandlerContext.channel();
 
-        // 打印客户端的远程地址
-        System.out.println(channel.remoteAddress());
+        if (httpObject instanceof HttpRequest) {
+            // 打印客户端的远程地址
+            System.out.println(channel.remoteAddress());
 
-        // 通过缓冲区定义发送的消息，读写数据都是通过缓冲区进行数据交换的
-        ByteBuf content = Unpooled.copiedBuffer("hello zrn", CharsetUtil.UTF_8);
+            // 通过缓冲区定义发送的消息，读写数据都是通过缓冲区进行数据交换的
+            ByteBuf content = Unpooled.copiedBuffer("hello zrn", CharsetUtil.UTF_8);
 
-        // 构建http的response
-        DefaultFullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
+            // 构建http的response
+            DefaultFullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
 
-        // 为响应添加数据类型和数据长度
-        response.headers().set(HttpHeaderNames.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-        response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+            // 为响应添加数据类型和数据长度
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
 
-        // 把响应数据放到缓冲区再刷到客户端
-        channelHandlerContext.writeAndFlush(response);
+            // 把响应数据放到缓冲区再刷到客户端
+            channelHandlerContext.writeAndFlush(response);
+        }
+
     }
 
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel注册...");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel移除...");
+        super.channelUnregistered(ctx);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel活跃...");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel不活跃...");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel读取数据完毕...");
+        super.channelReadComplete(ctx);
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        System.out.println("用户事件触发");
+        super.userEventTriggered(ctx, evt);
+    }
+
+    @Override
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel可写更改");
+        super.channelWritabilityChanged(ctx);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("捕获到异常");
+        super.exceptionCaught(ctx, cause);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handler添加...");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handler移除...");
+        super.handlerRemoved(ctx);
+    }
 }
